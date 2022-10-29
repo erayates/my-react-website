657376e65a60e1670986bd6eadfd5fdffa70bc2e
Eray Ateş <bligzer@outlook.com>
2022-10-30 02:30:27 +0300

    update

--- src/pages/Blog/index.js
+++ src/pages/Blog/index.js
@@ -3,16 +3,25 @@ import React from 'react'
 function Index() {
   return (
     <div className='w-3/4 mx-auto mt-5 bg-white rounded-lg' >
-        <div className='w-3/4 mx-auto mb-10 pt-8'>
+        <div className='w-3/4 mx-auto mb-6 pt-8'>
            <input type="search" className='w-full outline-none p-3 bg-transparent border mt-5 text-white shadow-md shadow-black/5 rounded-lg placeholder:text-slate-400/50 text-slate-700/50 text-sm' placeholder='Search a blog...'></input>
         </div> 
         <div className='flex flex-col absolute right-[100px] items-center w-1/3'>
-            <ul className='flex flex-col items-center w-[250px] mt-8'>
-                <li className='text-lg font-bold px-2 py-1 mb-3 w-full rounded bg-yellow-200 text-yellow-800 cursor-pointer hover:scale-105 border'>HTML5</li>
-                <li className='text-lg font-bold px-2 py-1 mb-3 w-full rounded bg-yellow-200 text-yellow-800 cursor-pointer hover:scale-105 border'>CSS3</li>
-                <li className='text-lg font-bold px-2 py-1 mb-3 w-full rounded bg-yellow-200 text-yellow-800 cursor-pointer hover:scale-105 border'>Javascript</li>
-                <li className='text-lg font-bold px-2 py-1 mb-3 w-full rounded bg-yellow-200 text-yellow-800 cursor-pointer hover:scale-105 border'>React</li>
+            <h3 className='text-slate-700 text-2xl mt-14 font-semibold'>Categories</h3>
+            <ul className='flex flex-col items-center w-[250px] mt-4'>
+                <li className='text-lg font-bold px-2 py-1 mb-3 w-full rounded bg-green-200 text-green-800 cursor-pointer hover:scale-105 border'>HTML5</li>
+                <li className='text-lg font-bold px-2 py-1 mb-3 w-full rounded bg-green-200 text-green-800 cursor-pointer hover:scale-105 border'>CSS3</li>
+                <li className='text-lg font-bold px-2 py-1 mb-3 w-full rounded bg-green-200 text-green-800 cursor-pointer hover:scale-105 border'>Javascript</li>
+                <li className='text-lg font-bold px-2 py-1 mb-3 w-full rounded bg-green-200 text-green-800 cursor-pointer hover:scale-105 border'>React</li>
             </ul>
+            <h3 className='text-slate-700 text-2xl mt-14 font-semibold'>Recently Added Posts</h3>
+            <ul className='flex flex-col items-center w-[250px] mt-4'>
+                <li className='text-md font-bold px-2 py-1 mb-3 w-full rounded bg-yellow-200 text-yellow-800 cursor-pointer hover:scale-105 border'>What is the HTML5?</li>
+                <li className='text-md font-bold px-2 py-1 mb-3 w-full rounded bg-yellow-200 text-yellow-800 cursor-pointer hover:scale-105 border'>When you need to use CSS3?</li>
+                <li className='text-md font-bold px-2 py-1 mb-3 w-full rounded bg-yellow-200 text-yellow-800 cursor-pointer hover:scale-105 border'>Javascript</li>
+                <li className='text-md font-bold px-2 py-1 mb-3 w-full rounded bg-yellow-200 text-yellow-800 cursor-pointer hover:scale-105 border'>React</li>
+            </ul>
+
         </div>
         <div className="flex justify-start items-center mb-20 p-10">
           <div className="h-full w-8/12 flex flex-col justify-center items-center rounded-lg p-5">
